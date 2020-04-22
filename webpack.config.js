b6d1e6143f64e19e99e0e74fbe6fc5c5b188296a
Albert Panesse <albert.panesse@gmail.com>
2020-04-22 10:36:28 +0700

    update!

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-	mode: 'development',
+	mode: 'production',
 	entry: {
     app: './src/main.js'
   },
